be66f5fe3f59913443499b901f06a25a7d0fd822
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-05-13 12:31:12 +0530

    ADDED : Feature To Return Minimum Fare For Less Distance & Time

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -1,8 +1,13 @@
 public class InvoiceGenerator {
+    private static final double MIN_FARE = 5.0;
     private final int COST_PER_TIME = 1;
     private final double Minimum_Cost_Per_Kilometer = 10;
 
     public double calculateFare(double distance, int time) {
-        return distance * Minimum_Cost_Per_Kilometer + time * COST_PER_TIME;
+        double totalfare = distance * Minimum_Cost_Per_Kilometer + time * COST_PER_TIME;
+        if(totalfare < MIN_FARE) {
+            return MIN_FARE;
+        }
+        return totalfare;
     }
 }
